f230a8d4d1afbe5ee9a809ef602206113520141d
Mike Abernathy <mabernathy87@gmail.com>
2026-03-19 15:58:58 -0600

    feat: update barrel exports for new components (step 5, part 4/5)

--- apps/pcpc/src/lib/components/index.ts
+++ apps/pcpc/src/lib/components/index.ts
@@ -3,3 +3,6 @@ export { default as CardSearchSelect } from './CardSearchSelect.svelte';
 export { default as CardVariantSelector } from './CardVariantSelector.svelte';
 export { default as SetDropdownItem } from './SetDropdownItem.svelte';
 export { default as SetGroupHeader } from './SetGroupHeader.svelte';
+export { default as SearchForm } from './SearchForm.svelte';
+export { default as CardDetailPanel } from './CardDetailPanel.svelte';
+export { default as PricingPanel } from './PricingPanel.svelte';
